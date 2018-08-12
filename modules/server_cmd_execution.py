131be6699419f723648b1dd99f91f8915f82b33e
Polina-Goroshko <40487792+Polina-Goroshko@users.noreply.github.com>
2018-08-12 17:30:05 +0300

    Create server_cmd_execution.py

---
+++ modules/server_cmd_execution.py
@@ -0,0 +1,18 @@
+"""Own modules: runs server commmands"""
+import subprocess
+import getpass
+import shlex
+
+
+class ServerCmdExecution:
+    """Define a function, which runs commands on a server"""
+
+    @staticmethod
+    def run_cmd(cmd: str) -> int:
+        """Run commands on a server"""
+        if getpass.getuser() == 'root':
+            cmd.replace('sudo', '')
+        proc = subprocess.Popen(shlex.split(cmd), stdin=subprocess.PIPE)
+        proc.communicate()
+        return_code = proc.wait()
+        return return_code
